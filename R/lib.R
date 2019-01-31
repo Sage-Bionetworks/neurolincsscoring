69750ec8885c2243001462355eed085cb69f224e
kdaily <kenneth.daily@sagebase.org>
2019-01-31 15:44:59 -0800

    move fxns to lib

--- R/lib.R
+++ R/lib.R
@@ -28,3 +28,60 @@ score_perfect_tracks <- function(d) {
 
   return(res)
 }
+
+#' Get censored wells from the censored well table.
+#'
+#' @param id Synapse ID of a censored well table.
+#'
+#' @return A data frame of censored well information.
+#' @export
+get_censored_wells <- function(id='syn11709601', ...) {
+  censored_wells <- readr::read_csv(synTableQuery(sprintf(query='select * from %s', id), ...)$filepath)
+}
+
+#' Identify object labels that are not present in image masks.
+#'
+#' These are potentially manual mistakes during curation.
+#'
+#' @param experiment A list that has an `experiment` name and `id` as a Synapse ID for a file with the survival data in it.
+#' @param original_data A data frame of potential objects.
+#'
+#' @return A data frame summary of the objects not present.
+#' @export
+find_manual_errors <- function(experiment, objects_data_frame) {
+  orig <- objects_data_frame %>% filter(Experiment == experiment$experiment)
+
+  f <- synGet(experiment$id)
+  d <- readr::read_csv(f$path, col_types = readr::cols(.default = "c"))
+
+  manual_timepoint_columns <- colnames(d)[stringr::str_detect(colnames(d), "T[0-9]+")]
+
+  # get manual time columns into a single column
+  d2 <- d %>%
+    tidyr::gather(key = "TimePointCol",
+                  "TimePointObject",
+                  one_of(manual_timepoint_columns))
+
+  d3 <- d2 %>%
+    mutate(TimePointCol=as.numeric(stringr::str_remove(TimePointCol, "T")),
+           TimePointObject=as.numeric(TimePointObject)) %>%
+    filter(Phenotype == "N") %>%
+    select(Sci_WellID, ObjectLabelsFound, Timepoint,
+           Time, TimePointCol, TimePointObject) %>%
+    filter(!is.na(TimePointObject),
+           !(TimePointObject %in% c("U")))
+
+  d4 <- anti_join(d3, orig,
+                  by=c("Sci_WellID"="Well",
+                       "TimePointObject"="ObjectLabelsFound"))
+
+  d5 <- d4 %>%
+    mutate(Experiment=experiment$experiment) %>%
+    select(Experiment,
+           Well=Sci_WellID,
+           ObjectLabelsFoundT0=ObjectLabelsFound,
+           TimePointColFromSurvival=TimePointCol,
+           ObjectLabelFromSurvival=TimePointObject) %>%
+    arrange(Well)
+  d5
+}
--- exec/survival-file-manual-errors.R
+++ exec/survival-file-manual-errors.R
@@ -1,61 +1,25 @@
 library(synapser)
 library(tidyverse)
+library(neurolincsscoring)
 
 synLogin()
 
+# Get experiment list from table
+# This has survival file entity ids in it too
 experiment_table_id <- 'syn11817859'
 experiment_table <- readr::read_csv(synTableQuery(sprintf('select Experiment,survival_file from %s WHERE survival_file is not NULL', experiment_table_id))$filepath) %>%
   select(-ROW_ID, -ROW_VERSION, experiment=Experiment, id=survival_file)
 
 experiments <- purrr::pmap(experiment_table, list)
 
+# Get censored wells data
 censored_wells_id <- 'syn11709601'
-censored_wells <- readr::read_csv(synTableQuery('select * from syn11709601')$filepath) %>%
-  select(-ROW_ID, -ROW_VERSION)
+censored_wells <- neurolincsscoring::get_censored_wells(censored_wells_id)
 
-original_data <- readr::read_csv("/home/kdaily/repositories/neurolincsdreamchallenge/workflows/unique-objects-workflow/unique-objects.csv")
+# This is a 3GB file. Read into memory with caution.
+objects_df <- readr::read_csv("/home/kdaily/repositories/neurolincsdreamchallenge/workflows/unique-objects-workflow/unique-objects.csv")
 
-experiment <- experiments[[1]]
-
-find_manual_errors <- function(experiment, original_data) {
-  orig <- original_data %>% filter(Experiment == experiment$experiment)
-
-  f <- synGet(experiment$id)
-  d <- readr::read_csv(f$path)
-
-  manual_timepoint_columns <- colnames(d)[stringr::str_detect(colnames(d), "T[0-9]+")]
-
-  # get manual time columns into a single column
-  d2 <- d %>%
-    tidyr::gather(key = "TimePointCol",
-                  "TimePointObject",
-                  one_of(manual_timepoint_columns))
-
-  d3 <- d2 %>%
-    mutate(TimePointCol=as.numeric(stringr::str_remove(TimePointCol, "T")),
-           TimePointObject=as.numeric(TimePointObject)) %>%
-    filter(Phenotype == "N") %>%
-    select(Sci_WellID, ObjectLabelsFound, Timepoint,
-           Time, TimePointCol, TimePointObject) %>%
-    filter(!is.na(TimePointObject),
-           !(TimePointObject %in% c("U")))
-
-  d4 <- anti_join(d3, orig,
-                  by=c("Sci_WellID"="Well",
-                       "TimePointObject"="ObjectLabelsFound"))
-
-  d5 <- d4 %>%
-    mutate(Experiment=experiment$experiment) %>%
-    select(Experiment,
-           Well=Sci_WellID,
-           ObjectLabelsFoundT0=ObjectLabelsFound,
-           TimePointColFromSurvival=TimePointCol,
-           ObjectLabelFromSurvival=TimePointObject) %>%
-    arrange(Well)
-  d5
-}
-
-foo <- purrr::map_df(experiments, find_manual_errors, original_data=original_data)
+foo <- purrr::map_df(experiments, neurolincsscoring::find_manual_errors, objects_data_frame = objects_df)
 foo <- anti_join(foo, censored_wells)
 readr::write_csv(foo, "/tmp/potential-manual-errors.csv")
 synStore(File("/tmp/potential-manual-errors.csv", parentId="syn17079347"))
