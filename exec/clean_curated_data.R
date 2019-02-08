4fa1823ad8fb2f773a93a163ffc5f466a61b9a02
kdaily <kenneth.daily@sagebase.org>
2019-02-07 16:19:24 -0800

    add script to clean curated data so it can be scored without modifying the table.

---
+++ exec/clean_curated_data.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+library(synapser)
+library(tidyverse)
+library(neurolincsscoring)
+
+foo <- synLogin()
+
+manuallyCuratedId <- 'syn11378063'
+
+curatedData <- curatedDataRaw %>%
+  filter(!is.na(ObjectTrackID), !Lost_Tracking) %>%
+  distinct()
+
+curatedData <- curatedData %>%
+  count(Experiment, Well, TimePoint, ObjectLabelsFound, ObjectTrackID) %>%
+  filter(n > 1) %>%
+  anti_join(curatedData, .,
+            by=c("Experiment", "Well", "TimePoint", "ObjectLabelsFound", "ObjectTrackID"))
+
+curatedData <- curatedData %>%
+  count(Experiment, Well, TimePoint, XCoordinate, YCoordinate) %>%
+  filter(n > 1) %>%
+  anti_join(curatedData, .,
+            by=c("Experiment", "Well", "TimePoint", "XCoordinate", "YCoordinate"))
+
+## ----remove-after-t-zero-------------------------------------------------
+curatedData <- curatedData %>%
+  group_by(Experiment, Well, ObjectTrackID) %>%
+  summarize(mintime = min(TimePoint)) %>%
+  ungroup() %>%
+  filter(mintime > 0) %>%
+  anti_join(curatedData, .,
+            by=c("Experiment", "Well", "ObjectTrackID"))
