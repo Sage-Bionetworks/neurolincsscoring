b683ca52be239326e3b3530538652f8a84f1a01b
Phil Snyder <subfivemile@gmail.com>
2019-02-23 19:44:57 -0800

    Validate tracking file before scoring, fail gracefully
    The scoring script now acts as both  a validator and a scorer. If the
    inputted tracking file is invalid, a json with fields "status" (=
    "INVALID"),
    "invalid_reasons" (A list), and "results" (null) is returned. If the tracking file is valid, a json
    with fields "status" (= "SCORED"), "invalid_reasons" (null), and
    "results" is returned.

--- exec/tracking_summary_perfect_tracks.R
+++ exec/tracking_summary_perfect_tracks.R
@@ -8,88 +8,137 @@ suppressPackageStartupMessages(library(optparse))
 suppressPackageStartupMessages(library(jsonlite))
 suppressPackageStartupMessages(library(neurolincsscoring))
 
-option_list <- list(
-  make_option(c("--tracking_file"), type = "character",
-              help = "Path or Synapse ID to tracking file.",
-              dest = "tracking_file"),
-  make_option(c("--curated_data_table"), type = "character",
-              help = "Path or ID containing curated data.",
-              dest = "curated_file",
-              default = "syn18344955"),
-  make_option(c("--per_well"), type = "logical",
-              action = "store_true",
-              help = "Report results per well instead of across all wells.",
-              dest = "json",
-              default = FALSE)
-  make_option(c("--json"), type = "logical",
-              action = "store_true",
-              help = "Write output in JSON format.",
-              dest = "json",
-              default = FALSE)
-)
-
-opt <- parse_args(OptionParser(option_list = option_list))
-
-if (stringr::str_detect(opt$tracking_file, "^syn.*")) {
-  syn_tracking_file <- TRUE
-  get_tracking_file_fxn <- neurolincsscoring::syn_get_tracking_submission_file
-} else {
-  syn_tracking_file <- FALSE
-  get_tracking_file_fxn <- neurolincsscoring::read_tracking_submission_file
+read_args <- function() {
+  option_list <- list(
+    make_option(c("--tracking_file"), type = "character",
+                help = "Path or Synapse ID to tracking file.",
+                dest = "tracking_file"),
+    make_option(c("--curated_data_table"), type = "character",
+                help = "Path or ID containing curated data.",
+                dest = "curated_file",
+                default = "syn18344955"),
+    make_option(c("--per_well"), type = "logical",
+                action = "store_true",
+                help = "Report results per well instead of across all wells.",
+                dest = "per_well",
+                default = FALSE))
+  opt <- parse_args(OptionParser(option_list = option_list))
+  return(opt)
 }
 
-if (stringr::str_detect(opt$curated_file, "^syn.*")) {
-  syn_curated_file <- TRUE
-  get_curated_data_fxn <- neurolincsscoring::syn_get_curated_data
-} else {
-  syn_curated_file <- FALSE
-  get_curated_data_fxn <- neurolincsscoring::read_curated_data
+get_tracking_file_reader <- function(tracking_file_path) {
+  tracking_file_reader <- list(isSynapseId = FALSE, read = NULL)
+  if (stringr::str_detect(tracking_file_path, "^syn.*")) {
+    tracking_file_reader$isSynapseId <- TRUE
+    tracking_file_reader$read <- neurolincsscoring::syn_get_tracking_submission_file
+  } else {
+    tracking_file_reader$isSynapseId <- FALSE
+    tracking_file_reader$read <- readr::read_csv
+  }
+  return(tracking_file_reader)
 }
 
-if (syn_tracking_file | syn_curated_file) {
-  foo <- capture.output(synLogin())
+get_curated_file_reader <- function(curated_file_path) {
+  curated_file_reader <- list(isSynapseId = FALSE, read = NULL)
+  if (stringr::str_detect(curated_file_path, "^syn.*")) {
+    curated_file_reader$isSynapseId <- TRUE
+    curated_file_reader$read <- neurolincsscoring::syn_get_curated_data
+  } else {
+    curated_file_reader$isSynapseId <- FALSE
+    curated_file_reader$read <- neurolincsscoring::read_curated_data
+  }
+  return(curated_file_reader)
 }
 
-## ----get-tracking-results, message = FALSE---------------------------------
-trackingResults <- get_tracking_file_fxn(opt$tracking_file)
-
-testexpts <- assertthat::assert_that(length(unique(trackingResults$Experiment)) == 1,
-                                     msg = "Did not find a single experiment in tracking file.")
-
-## ----get-curated-data----------------------------------------------------
-curatedData <- get_curated_data_fxn(opt$curated_file)
-
-trackingResults <- trackingResults %>%
-  assertr::verify(trackingResults$Experiment %in% curatedData$Experiment)
-
-curatedData <- curatedData %>%
-  dplyr::filter(Experiment %in% unique(trackingResults$Experiment))
-
-message(sprintf("Curated data has %s rows\n", nrow(curatedData)))
-message(sprintf("Tracking submission has %s rows\n", nrow(trackingResults)))
-
-## ----merge-curated-and-tracked-------------------------------------------
-merged <- dplyr::full_join(curatedData,
-                           trackingResults,
-                           by = c("Experiment" = "Experiment",
-                                  "Well" = "Well",
-                                  "TimePoint" = "TimePoint",
-                                  "ObjectLabelsFound" = "ObjectLabelsFound"))
-
-merged <- merged %>%
-  dplyr::mutate(matched = ObjectTrackID.x == ObjectTrackID.y)
-
-merged$matched[is.na(merged$matched)] <- FALSE
-
-## ----percentagetable-expt-well-object------------------------------------
-if (opt$per_well) {
-  res <- neurolincsscoring::score_perfect_tracks_per_well(merged)
-} else {
-  res <- neurolincsscoring::score_perfect_tracks(merged)
-  check_n_rows <- assertthat::assert_that(nrow(res) == 1, msg = "Number of rows in result is not equal to 1.")
+conditional_synLogin <- function(syn_tracking_file, syn_curated_file) {
+  if (syn_tracking_file | syn_curated_file) {
+    synLogin(silent = TRUE)
+  }
 }
-if (opt$json) {
-  cat(jsonlite::toJSON(as.list(res), auto_unbox = TRUE))
-} else {
-  cat(readr::format_csv(res))
+
+read_tracking_file <- function(tracking_file_path, read) {
+  trackingResults <- tryCatch({
+    readr::stop_for_problems(
+      read(tracking_file_path))
+  }, error = function(e) {
+    return("The tracking file is not readable as a csv format.")
+  })
+  return(trackingResults)
 }
+
+read_curated_table <- function(curated_table_path, read) {
+  curatedData <- tryCatch({
+    readr::stop_for_problems(
+      read(curated_table_path))
+  }, error = function(e) {
+    return("The curated data table is not readable as a csv format.")
+  })
+  return(curatedData)
+}
+
+cat_invalid_reasons <- function(invalid_reasons) {
+  cat(jsonlite::toJSON(list(status = "INVALID", invalid_reasons = invalid_reasons)))
+}
+
+score_tracking_results <- function(trackingResults, curatedData, per_well) {
+  curatedData <- curatedData %>%
+    dplyr::filter(Experiment %in% unique(trackingResults$Experiment))
+  message(sprintf("Curated data has %s rows\n", nrow(curatedData)))
+  message(sprintf("Tracking submission has %s rows\n", nrow(trackingResults)))
+  merged <- dplyr::full_join(curatedData,
+                             trackingResults,
+                             by = c("Experiment" = "Experiment",
+                                    "Well" = "Well",
+                                    "TimePoint" = "TimePoint",
+                                    "ObjectLabelsFound" = "ObjectLabelsFound"))
+
+  merged <- merged %>%
+    dplyr::mutate(matched = ObjectTrackID.x == ObjectTrackID.y)
+
+  merged$matched[is.na(merged$matched)] <- FALSE
+
+  if (per_well) {
+    res <- neurolincsscoring::score_perfect_tracks_per_well(merged)
+  } else {
+    res <- neurolincsscoring::score_perfect_tracks(merged)
+    check_n_rows <- assertthat::assert_that(
+      nrow(res) == 1, msg = "Number of rows in result is not equal to 1.")
+  }
+  cat(jsonlite::toJSON(
+    list(
+      status = "SCORED",
+      invalid_reasons = NULL,
+      results = as.list(res)),
+    auto_unbox = TRUE))
+}
+
+main <- function() {
+  opt <- read_args()
+  invalid_reasons = NULL # If the tracking file is invalid, we will output this value as json
+  tracking_file_reader <- get_tracking_file_reader(opt$tracking_file)
+  curated_file_reader <- get_curated_file_reader(opt$curated_file)
+  conditional_synLogin(tracking_file_reader$isSynapseId,
+                       curated_file_reader$isSynapseId)
+
+  trackingResults <- read_tracking_file(opt$tracking_file, tracking_file_reader$read)
+  if (is.character(trackingResults)) { # returned invalid reason
+    cat_invalid_reasons(trackingResults)
+    return()
+  }
+
+  curatedData <- read_curated_table(opt$curated_file, curated_file_reader$read)
+  if (is.character(curatedData)) { # returned invalid reason
+    cat_invalid_reasons(curatedData)
+    return()
+  }
+
+  invalid_reasons <- neurolincsscoring::validate_tracking_results(
+                         trackingResults, curatedData)
+  if (length(invalid_reasons)) {
+    cat_invalid_reasons(invalid_reasons)
+    return()
+  }
+  score_tracking_results(trackingResults, curatedData, opt$per_well)
+}
+
+main()
